33c5b225cf0855bd112a2687794730fa24653712
23-mid-0347 <shyamprasadtks@gmail.com>
2025-11-07 11:56:05 +0530

    Fixed environment variable MongoDB connection

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ app = FastAPI()
 # --- MongoDB Connection (Atlas via Environment Variables) ---
 MONGO_URI = os.getenv("MONGO_URI")
 DB_NAME = os.getenv("DB_NAME")
+print("MONGO_URI:", os.getenv("MONGO_URI"))
+print("DB_NAME:", os.getenv("DB_NAME"))
 
 if not MONGO_URI:
     raise ValueError("❌ MONGO_URI environment variable not found.")
